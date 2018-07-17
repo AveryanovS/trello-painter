3976b4300f63364277afa74dc18e6580c960c87f
AveryanovS <averyanov@my.com>
2018-07-17 18:18:28 +0300

    working version

--- painter.js
+++ painter.js
@@ -1,20 +1,21 @@
 var KEYWORDS_COLORS = {
-    'todo': '#ffb3b3',
-    'done': '#9fdf9f',
-    'баги': '#ffdd99d'
+    'баги': '#ffb3b3',
+    'todo': '#ffdd99'
 };
-var lists = document.getElementsByClassName('js-list list-wrapper');
-for(var i = 0; i < lists.length; i++){
-    var list = lists[i]
-        .children[0];
-    var text = list
-        .children[0]
-        .children[1]
-        .innerText
-        .toLowerCase();
-    for(let keyword in KEYWORDS_COLORS){
-        if(text.includes(keyword))
-            list.style.backgroundColor = KEYWORDS_COLORS[keyword];
-    }
-    
-}   
+var lists = document.getElementsByClassName('js-list');
+window.onload = () => {
+    for(var i = 0; i < lists.length; i++){
+        var list = lists[i]
+            .children[0];
+        var text = list
+            .children[0]
+            .children[1]
+            .innerText
+            .toLowerCase();
+        for(let keyword in KEYWORDS_COLORS){
+            if(text.includes(keyword))
+                list.style.backgroundColor = KEYWORDS_COLORS[keyword];
+        }
+        
+    }   
+};
