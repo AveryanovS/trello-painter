888e69d06abb6ad3e2989264f247f4eb557999a2
AveryanovS <averyanov@my.com>
2018-07-17 18:38:23 +0300

    load keywords from json

--- painter.js
+++ painter.js
@@ -1,21 +1,26 @@
-var KEYWORDS_COLORS = {
-    'баги': '#ffb3b3',
-    'todo': '#ffdd99'
-};
 var lists = document.getElementsByClassName('js-list');
+var xhr = new XMLHttpRequest();
 window.onload = () => {
-    for(var i = 0; i < lists.length; i++){
-        var list = lists[i]
-            .children[0];
-        var text = list
-            .children[0]
-            .children[1]
-            .innerText
-            .toLowerCase();
-        for(let keyword in KEYWORDS_COLORS){
-            if(text.includes(keyword))
-                list.style.backgroundColor = KEYWORDS_COLORS[keyword];
+    xhr.open('GET', chrome.extension.getURL('/keywords.json'), true);
+    xhr.onreadystatechange = () => { 
+        if (xhr.readyState != 4) 
+            return;
+        if (xhr.status === 200) {
+            var keywordColors = JSON.parse(xhr.responseText);
+            for(var i = 0; i < lists.length; i++){
+                var list = lists[i]
+                    .children[0];
+                var text = list
+                    .children[0]
+                    .children[1]
+                    .innerText
+                    .toLowerCase();
+                for(let keyword in keywordColors){
+                    if(text.includes(keyword))
+                        list.style.backgroundColor = keywordColors[keyword];
+                }
+            }
         }
-        
-    }   
+    };   
+    xhr.send();
 };
