c90f23fc50c9344fded61fe5be576c5c649cec3f
AveryanovS <averyanov@my.com>
2018-07-18 19:03:17 +0300

    transparency

--- painter.js
+++ painter.js
@@ -5,22 +5,33 @@ window.onload = () => {
     xhr.onreadystatechange = () => { 
         if (xhr.readyState != 4) 
             return;
-        if (xhr.status === 200) {
-            var keywordColors = JSON.parse(xhr.responseText);
-            for(var i = 0; i < lists.length; i++){
-                var list = lists[i]
-                    .children[0];
-                var text = list
-                    .children[0]
-                    .children[1]
-                    .innerText
-                    .toLowerCase();
-                for(let keyword in keywordColors){
-                    if(text.includes(keyword))
-                        list.style.backgroundColor = keywordColors[keyword];
-                }
-            }
+        if (xhr.status !== 200) 
+            return;
+        var dictionary;    
+        try {
+            dictionary = JSON.parse(xhr.responseText);
+        } catch (error){
+            return;
         }
+        parse(dictionary);
     };   
     xhr.send();
 };
+
+function parse(dictionary){
+    for(var list of Array.from(lists)){
+        var text = list.getElementsByClassName('list-header-name-assist')[0].innerText.toLowerCase();
+        list = list.children[0];
+        console.log(text);
+        for(let keyword in dictionary.colors){
+            if(text.includes(keyword)){
+                list.style.backgroundColor = dictionary.colors[keyword];
+            }
+        }
+        for(let keyword in dictionary.transparency){
+            if(text.includes(keyword)){
+                list.style.opacity = dictionary.transparency[keyword];
+            }
+        }
+    }
+}
